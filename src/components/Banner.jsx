65263ec18e5442ebae855cbbe1ff86e545708ed2
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-01-27 18:29:42 +0530

    banner modified

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,7 +14,7 @@ export const CircularText = () => {
       ))}
       <div className="icon-container">
         <Link to={"https://youtu.be/mVtUlRpoCzs?si=rlYCdm9FWH6z5BMd"}>
-        <i class="fa-solid fa-play"></i>
+          <i className="fa-solid fa-play"></i>
         </Link>
       </div>
     </div>
@@ -25,16 +25,18 @@ function Banner() {
   const [textIndex, setTextIndex] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
 
-  const sliderTexts = ["better", "higher paying", "exciting", "more fun job"];
+  const sliderTexts = ["better job", "higher paying job", "exciting job", "more fun job"];
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setIsAnimating(true);
+      setIsAnimating(true);  // Trigger animation
       setTimeout(() => {
         setTextIndex((prevIndex) => (prevIndex + 1) % sliderTexts.length);
-        setIsAnimating(false);
-      }, 600); 
-    }, 5000); 
-    return () => clearInterval(interval); 
+        setIsAnimating(false); // Stop animation after text change
+      }, 400); // Animation duration (400ms)
+    }, 2000); // Change text every 2 seconds
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -43,25 +45,36 @@ function Banner() {
       <div className="banner">
         <div className="content">
           <div className='content-1'>
-            <img src="
-            https://infolio1.themescamp.com/creative-agency/wp-content/uploads/sites/5/2023/12/02.jpg" alt=""  className='left-img-banner'/>
+            <img
+              src="https://infolio1.themescamp.com/creative-agency/wp-content/uploads/sites/5/2023/12/02.jpg"
+              alt=""
+              className='left-img-banner'
+            />
             <div>
               <h4 className='content-1-text'>6K+</h4>
               <p className='content-1-desc'>SATISFIED CLIENTS</p>
             </div>
           </div>
           <div className='content-2'>
-            <h1 className='content-text'>Land a <span className={`highlight ${
-                  isAnimating ? "tracking-out-contract" : "tracking-in-expand"
-                }`}>{sliderTexts[textIndex]}</span></h1>
-            <p className='content-desc'>Your dream job is out there – and it’s hiring!
-              <br/>
-            Ready for something new, exciting, and rewarding?
+            <h1 className='content-text'>
+              Land a{" "}
+              <span className={`highlight ${isAnimating ? "" : ""}`}>
+                {sliderTexts[textIndex]}
+              </span>
+            </h1>
+            <p className='content-desc'>
+              Your dream job is out there – and it’s hiring!
+              <br />
+              Ready for something new, exciting, and rewarding?
             </p>
           </div>
           <div className='content-3'>
-            <CircularText/>
-            <img src="https://infolio1.themescamp.com/creative-agency/wp-content/uploads/sites/5/2023/12/03.jpg" alt="" className='right-img-banner' />
+            <CircularText />
+            <img
+              src="https://infolio1.themescamp.com/creative-agency/wp-content/uploads/sites/5/2023/12/03.jpg"
+              alt=""
+              className='right-img-banner'
+            />
           </div>
         </div>
       </div>
