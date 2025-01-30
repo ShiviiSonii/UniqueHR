5ae58b8bc66ea6ee668de0513bc7d3ed965ed771
ShiviiSonii <1111dhruvsoni@gmail.com>
2025-01-30 11:10:36 +0530

    about page changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,16 @@ import Navbar from '../components/Navbar';
 import AboveFooter from '../components/AboveFooter';
 import Footer from '../components/Footer';
 import CommonBanner from '../components/CommonBanner';
+import Testimonial from '../components/Testimonial';
+import Blogs from '../components/Blogs';
 
 function About() {
   return (
     <>
       <Navbar />
       <CommonBanner page="About"/>
+      <Testimonial/>
+      <Blogs/>
       <AboveFooter />
       <Footer />
     </>
